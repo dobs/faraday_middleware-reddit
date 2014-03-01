8215a797cf0d9729bf108d7b3001c8a27f555220
Daniel O'Brien <dan@dobs.org>
2014-03-01 16:45:41 -0500

    Initial commit.

---
+++ lib/faraday_middleware/reddit_api.rb
@@ -0,0 +1,7 @@
+require "faraday_middleware/reddit_api/version"
+
+module FaradayMiddleware
+  module RedditApi
+    # Your code goes here...
+  end
+end
---
+++ lib/faraday_middleware/reddit_api/version.rb
@@ -0,0 +1,5 @@
+module FaradayMiddleware
+  module RedditApi
+    VERSION = "0.0.1"
+  end
+end
