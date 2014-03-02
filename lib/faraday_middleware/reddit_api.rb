d7b409473e702670c16f6636f346929d1b685f8d
Daniel O'Brien <dan@dobs.org>
2014-03-02 00:51:33 -0500

    Initial RateLimit middleware.

--- lib/faraday_middleware/reddit_api.rb
+++ lib/faraday_middleware/reddit_api.rb
@@ -2,13 +2,15 @@ require 'faraday'
 
 module FaradayMiddleware
   module RedditApi
-    autoload :Modhash,              'faraday_middleware/reddit_api/request/modhash'
     autoload :Authentication, 'faraday_middleware/reddit_api/request/authentication'
+    autoload :Modhash,        'faraday_middleware/reddit_api/request/modhash'
+    autoload :RateLimit,      'faraday_middleware/reddit_api/request/rate_limit'
 
     if Faraday.respond_to? :register_middleware
       Faraday.register_middleware :request,
+        :reddit_authentication => lambda { Authentication },
         :reddit_modhash        => lambda { Modhash },
-        :reddit_authentication => lambda { Authentication }
+        :reddit_rate_limit     => lambda { RateLimit }
     end
   end
 end
