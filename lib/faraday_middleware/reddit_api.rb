cc6dec1c7894846cabd24d0b359226ed31082a87
Daniel O'Brien <dan@dobs.org>
2014-03-02 12:02:19 -0500

    Adding ForceJson middleware.

--- lib/faraday_middleware/reddit_api.rb
+++ lib/faraday_middleware/reddit_api.rb
@@ -3,12 +3,14 @@ require 'faraday'
 module FaradayMiddleware
   module RedditApi
     autoload :Authentication, 'faraday_middleware/reddit_api/request/authentication'
+    autoload :ForceJson,      'faraday_middleware/reddit_api/request/force_json'
     autoload :Modhash,        'faraday_middleware/reddit_api/request/modhash'
     autoload :RateLimit,      'faraday_middleware/reddit_api/request/rate_limit'
 
     if Faraday.respond_to? :register_middleware
       Faraday.register_middleware :request,
         :reddit_authentication => lambda { Authentication },
+        :reddit_force_json     => lambda { ForceJson },
         :reddit_modhash        => lambda { Modhash },
         :reddit_rate_limit     => lambda { RateLimit }
     end
---
+++ lib/faraday_middleware/reddit_api/request/force_json.rb
@@ -0,0 +1,29 @@
+require 'faraday'
+
+module FaradayMiddleware::RedditApi
+  # Request middleware to force reddit to provide JSON responses when
+  # available.
+  #
+  # For GET requests that means adding `.json` to  the ends of URLs. For POST
+  # requests that means adding `api_type=json` to sent requests.
+  class ForceJson < Faraday::Middleware
+    def initialize(app, options = nil)
+      super(app)
+      @options = options || {}
+    end
+
+    def call(env)
+      if env[:method].to_s == 'get'
+        env[:url].path += '.json' unless env[:url].path.end_with?('.json')
+      elsif env[:method].to_s == 'post'
+        if env[:body].is_a? Hash
+          env[:body][:api_type] = 'json'
+        else
+          env[:body] = [env[:body], 'api_type=json'].compact.join('&')
+        end
+      end
+
+      @app.call(env)
+    end
+  end
+end
