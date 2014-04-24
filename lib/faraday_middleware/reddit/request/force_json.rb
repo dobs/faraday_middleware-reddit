cbe2f06deab1bcf4aabadfc4fa462fcc896f346e
Daniel O'Brien <dan@dobs.org>
2014-04-23 20:39:48 -0400

    POST vs. GET sensitivity in force_json middlewawre.

--- lib/faraday_middleware/reddit/request/force_json.rb
+++ lib/faraday_middleware/reddit/request/force_json.rb
@@ -1,25 +1,27 @@
 require 'faraday'
 
-module FaradayMiddleware::Reddit
-  # Request middleware to force reddit to provide JSON responses when
-  # available.
-  #
-  # For GET requests that means adding `.json` to  the ends of URLs. For POST
-  # requests that means adding `api_type=json` to sent requests.
-  class ForceJson < Faraday::Middleware
-    def initialize(app, options = nil)
-      super(app)
-      @options = options || {}
-    end
-
-    def call(env)
-      if env[:method].to_s == 'get'
-        env[:url].path += '.json' unless env[:url].path.end_with?('.json')
-      elsif env[:method].to_s == 'post'
-        env[:body] = [env[:body], 'api_type=json'].compact.join('&')
+module FaradayMiddleware
+  module Reddit
+    # Request middleware to force reddit to provide JSON responses when
+    # available.
+    #
+    # For GET requests that means adding `.json` to  the ends of URLs. For POST
+    # requests that means adding `api_type=json` to sent requests.
+    class ForceJson < Faraday::Middleware
+      def initialize(app, options = nil)
+        super(app)
+        @options = options || {}
       end
 
-      @app.call(env)
+      def call(env)
+        if env[:method].to_s == 'get'
+          env[:url].path += '.json' unless env[:url].path.end_with?('.json')
+        elsif env[:method].to_s == 'post'
+          env[:body] = [env[:body], 'api_type=json'].compact.join('&')
+        end
+
+        @app.call(env)
+      end
     end
   end
 end
