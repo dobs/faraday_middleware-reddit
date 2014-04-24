62ce251318e4829699cb5dac3214f80bdb82bec3
Daniel O'Brien <dan@dobs.org>
2014-04-23 20:40:49 -0400

    Rake and initial spec helper.

---
+++ spec/helper.rb
@@ -0,0 +1,11 @@
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require 'rspec'
+require 'uri'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
