f70581c77cae2ad7ccc6af387fa0fa9d384cf386
Daniel O'Brien <daniel.obrien@strategiccoach.com>
2014-04-25 16:35:49 -0400

    Support for OAuth2 access tokens.

--- lib/faraday_middleware/reddit/request/authentication.rb
+++ lib/faraday_middleware/reddit/request/authentication.rb
@@ -18,31 +18,45 @@ module FaradayMiddleware
 
       def initialize(app, options)
         super(app)
-        @options = options
-        @user    = @options[:user]
-        @passwd  = @options[:password]
-        @rem     = @options[:remember]
-        @cookie  = @options[:cookie]
+        @options      = options
+        @user         = @options[:user]
+        @passwd       = @options[:password]
+        @rem          = @options[:remember]
+        @access_token = @options[:access_token]
+        @cookie       = @options[:cookie]
 
-        unless (@options[:user] && @options[:password]) || @options[:cookie]
-          fail ArgumentError, 'Either `user` and `password` or `cookie` need to be provided as options to the :reddit_authentication middleware'
+        unless (@options[:user] && @options[:password]) || @options[:cookie] || @options[:access_token]
+          fail ArgumentError, 'Either `user` and `password`, `cookie`, or `access_token` need to be provided as options to the :reddit_authentication middleware'
         end
       end
 
       def call(env)
-        authenticate(env) unless @cookie
-        apply_cookie(env)
+        if @access_token
+          apply_access_token(env)
+        elsif @cookie
+          apply_cookie(env)
+        else
+          authenticate(env)
+        end
+
         @app.call(env)
       end
 
-      def authenticate(env)
-        response = Faraday.post AUTH_URL, user: @user, passwd: @passwd, rem: @rem, api_type: 'json'
-        @cookie = response.headers['set-cookie']
+      def apply_access_token(env)
+        env.url.scheme = 'https'
+        env.url.port = 443
+        env.url.host = 'oauth.reddit.com'
+        env[:request_headers]['Authorization'] = "bearer #{@access_token}"
       end
 
       def apply_cookie(env)
         upstream_cookies = env[:request_headers]['Cookie']
         env[:request_headers]['Cookie'] = upstream_cookies ? "#{upstream_cookies}; #{@cookie}" : @cookie
+      end
+
+      def authenticate(env)
+        response = Faraday.post AUTH_URL, user: @user, passwd: @passwd, rem: @rem, api_type: 'json'
+        @cookie = response.headers['set-cookie']
       end
     end
   end
