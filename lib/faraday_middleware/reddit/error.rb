ef423006eb8d49e7a58e3f92f0dea5e0948b428a
Daniel O'Brien <dan@dobs.org>
2014-05-20 01:35:38 -0400

    More error types, different workflow for catching and raising.

--- lib/faraday_middleware/reddit/error.rb
+++ lib/faraday_middleware/reddit/error.rb
@@ -2,11 +2,48 @@ require 'faraday/error'
 
 module FaradayMiddleware
   module Reddit
-    class TooManyRequestsError    < Faraday::ClientError; end # HTTP Status 429
-    class BadGatewayError         < Faraday::ClientError; end # HTTP Status 502
-    class ServiceUnavailableError < Faraday::ClientError; end # HTTP Status 503
-    class GatewayTimeoutError     < Faraday::ClientError; end # HTTP Status 504
+    class ClientError < Faraday::ClientError; end
+    class ServerError < Faraday::ClientError; end
 
-    RETRIABLE_ERRORS = [ServiceUnavailableError, GatewayTimeoutError]
+    # Many of these errors are hypothetical, but better safe than sorry.
+    class BadRequestError            < ClientError; end
+    class UnauthorizedError          < ClientError; end
+    class ForbiddenError             < ClientError; end
+    class NotFoundError              < ClientError; end
+    class MethodNotAllowedError      < ClientError; end
+    class NotAcceptableError         < ClientError; end
+    class RequestTimeoutError        < ClientError; end
+    class RequestEntityTooLargeError < ClientError; end
+    class RequestURITooLongError     < ClientError; end
+    class UnsupportedMediaTypeError  < ClientError; end
+    class TooManyRequestsError       < ClientError; end
+    class InternalServerError        < ServerError; end
+    class NotImplementedError        < ServerError; end
+    class BadGatewayError            < ServerError; end
+    class ServiceUnavailableError    < ServerError; end
+    class GatewayTimeoutError        < ServerError; end
+
+    ERROR_CODES = {
+      400 => BadRequestError,
+      401 => UnauthorizedError,
+      403 => ForbiddenError,
+      404 => NotFoundError,
+      405 => MethodNotAllowedError,
+      406 => NotAcceptableError,
+      408 => RequestTimeoutError,
+      413 => RequestEntityTooLargeError,
+      414 => RequestURITooLongError,
+      429 => TooManyRequestsError,
+      500 => InternalServerError,
+      501 => NotImplementedError,
+      502 => BadGatewayError,
+      503 => ServiceUnavailableError,
+      504 => GatewayTimeoutError
+    }.freeze
+
+    RETRIABLE_ERRORS = [
+      ServiceUnavailableError,
+      GatewayTimeoutError
+    ].freeze
   end
 end
--- lib/faraday_middleware/reddit/response/raise_error.rb
+++ lib/faraday_middleware/reddit/response/raise_error.rb
@@ -8,18 +8,11 @@ module FaradayMiddleware
     # 429 for hitting the API rate limit or 504 for gateway timeouts.
     class RaiseError < Faraday::Response::RaiseError
       def on_complete(env)
-        case env[:status]
-        when 429
-          raise FaradayMiddleware::Reddit::TooManyRequestsError, response_values(env)
-        when 502
-          raise FaradayMiddleware::Reddit::BadGatewayError, response_values(env)
-        when 503
-          raise FaradayMiddleware::Reddit::ServiceUnavailableError, response_values(env)
-        when 504
-          raise FaradayMiddleware::Reddit::GatewayTimeoutError, response_values(env)
+        if FaradayMiddleware::Reddit::ERROR_CODES.include? env[:status]
+          raise FaradayMiddleware::Reddit::ERROR_CODES[env[:status]], response_values(env)
+        else
+          super
         end
-
-        super
       end
     end
   end
